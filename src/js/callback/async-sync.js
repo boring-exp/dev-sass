a3048fe9cdd19b8406fe4ad599a26656ca23372e
jinweios <jinwei.peng@outlook.com>
2022-11-26 16:18:25 +0800

    feat: callback

---
+++ src/js/callback/async-sync.js
@@ -0,0 +1,26 @@
+// 1.同步
+// 2.异步
+// 3.同步阻塞
+// 4.同步非阻塞
+
+
+// 异步的
+// callback hell
+setTimeout(() => {
+    console.log('身份信息')
+    setTimeout(() => {
+        console.log('获取了快递信息')
+        setTimeout(() => {
+            console.log('获取shunfen快递')
+            setTimeout(() => {
+                console.log('获取商品信息')
+                setTimeout(() => {
+                    console.log('获取价格信息')
+                }, 3000)
+            }, 3000)
+
+        }, 3000)
+    }, 3000)
+}, 3000)
+
+
