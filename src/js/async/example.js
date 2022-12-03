ab9afe62461da6a37ceef9b560bd949cdf6a00a4
jinweios <jinwei.peng@outlook.com>
2022-12-03 11:49:23 +0800

    feat: axios

--- src/js/async/example.js
+++ src/js/async/example.js
@@ -1,16 +1,30 @@
-setTimeout(() => {
-    console.log('hello world');
-    setTimeout(() => {
-        console.log('hello world');
-        setTimeout(() => {
-            console.log('hello world')
-        }, 2000)
-    }, 2000)
-}, 2000)
-
-
 const task = new Promise((resolve, reject) => {
-    // 怎么写
+    // 怎么写 >=2000 不是精确的定时器
+    setTimeout(() => {
+        resolve()
+    }, 2000)
 });
 
-task.then().then().then();
+task.then(() => {
+    console.log('hello world')
+    return new Promise((resolve, reject) => {
+        // 怎么写 >=2000 不是精确的定时器
+        setTimeout(() => {
+            resolve()
+        }, 2000)
+    });
+}).then(
+    () => {
+        console.log('hello world')
+        return new Promise((resolve, reject) => {
+            // 怎么写 >=2000 不是精确的定时器
+            setTimeout(() => {
+                resolve()
+            }, 2000)
+        });
+    }
+).then(
+    () => {
+        console.log('hello world')
+    }
+);
